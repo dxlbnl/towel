28f06828d20fb6f298d8aa963d1afc1afe5d2728
Dexter <a.esselink@gmail.com>
2012-11-21 20:14:01 -0500

    Forgot to add the chatserver file

---
+++ chatServer.py
@@ -0,0 +1,36 @@
+class ChatServer(object):
+    users = []
+    
+    def __init__(self, client):
+        
+        print "Creating chat", self.users
+        self.client = client
+        for i in range(10):
+            n = "Guest-{}".format(i)
+            if n not in self.users:
+                self.name = n
+                self.users.append(self.name)
+                break
+        
+        self.client.one.name_change(self.name)
+        
+        
+    def on_message(self, msg):
+        # notify all other users.
+        self.client.all.on_message(msg)
+        
+    def name_changed(self, name):
+        print "name changed", name
+        if self.name in self.users:
+            self.users.remove(self.name)
+            
+        self.name = name        
+        self.users.append(name)
+        self.users.sort()
+        
+        self.client.all.update_users(self.users)
+    
+    def detach(self):
+        self.users.remove(self.name)
+        self.client.all.update_users(self.users)
+
